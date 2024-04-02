3c791f388712ce454c31ed0ea3c2980079f299f3
Flimill <uhovad03@gmail.com>
2024-04-03 02:48:46 +0300

    final?

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 from PIL import Image, ImageDraw
 
 import numpy as np
+
+from plot_graph import save_graph
 np.set_printoptions(threshold=np.inf)
 
 
@@ -14,10 +16,7 @@ class HopfieldNetwork:
 
     def train(self, patterns):
         num_patterns = len(patterns)
-        train_count=0
         for pattern in patterns:
-            train_count+=1
-            print(f"train_count={train_count}")
             pattern = np.array(pattern)
             outer_product = np.outer(pattern, pattern)
             self.weights += outer_product / num_patterns  # Вычисление весов по формуле
@@ -25,10 +24,7 @@ class HopfieldNetwork:
         
     def predict(self, pattern, max_iters=100):
         pattern = np.array(pattern)
-        predict_count=0
         for _ in range(max_iters):
-            predict_count+=1
-            print(f"predict_count={predict_count}")
             new_pattern = np.sign(np.dot(self.weights, pattern))
             if np.array_equal(new_pattern, pattern):
                 return new_pattern
@@ -50,7 +46,6 @@ if __name__ == "__main__":
     train_path = "patterns"
     #shape_types=['box','circle','triangle']
     shape_types=['box']
-    noise_level = 0.1
     
     #generate_photo.genererate_pictures(train_path, num_samples,w,h,min_fig_size,num_shapes)
     
@@ -59,26 +54,30 @@ if __name__ == "__main__":
     # Предположим, что у нас есть три различных образца: квадрат, круг и треугольник.
     # Каждый образец представлен в виде бинарного вектора.
     patterns=[]
-    
-    for type in shape_types:
+    patterns=[]
+    start = 0
+    end = 100
+    step = 1
+    accuracy_array=[]
+    noise_level_array = []
+    for noise_level in range(int(start * 10), int(end * 10) + 1, int(step * 10)): 
+        noise_level_array.append(noise_level/1000)
+        true_predict_count= 0   
         for i in range(num_samples):
             path=f"patterns/{i}.png"
-            #image_binary_vector = generate_photo.image_path_to_binary_vector(path, threshold=128, new_width=w, new_height=h)
             image_binary_vector = generate_photo.image_path_to_vector(path,w,h)
             patterns.append(image_binary_vector)
 
-    network = HopfieldNetwork(pattern_size=len(patterns[0]))
-    network.train(patterns)
-    for i in range(num_samples):
-        for type in shape_types:
-            path=f"patterns/{i}.png"
-            generate_photo.vector_to_image(generate_photo.image_path_to_vector(path,w,h),w,h).show()
-            
-            noisy_image= generate_photo.get_noisy_picture(path,w,h, noise_level)
-            generate_photo.vector_to_image(generate_photo.image_to_vector(noisy_image,w,h),w,h).show()
+        network = HopfieldNetwork(pattern_size=len(patterns[0]))
+        network.train(patterns)
+        for i in range(num_samples):
+            path=f"patterns/{i}.png"        
+            noisy_image= generate_photo.get_noisy_picture(path,w,h, noise_level/1000)
             noisy_image_binary_vector = generate_photo.image_to_vector(noisy_image,w,h)
             predicted_pattern=network.predict(noisy_image_binary_vector)
             result_image = generate_photo.vector_to_image(predicted_pattern, w, h)
-            result_image.show()
-            time.sleep(2)
+        accuracy = true_predict_count/(len(shape_types) * num_samples)
+        print(f"Зашумлённость = {noise_level/10}%, accuracy = {accuracy*100}%")
+        accuracy_array.append(accuracy)
+    save_graph(noise_level_array, accuracy_array, "noise_level")
 
